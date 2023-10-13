8851c2b5326b68c5311948e6db2ea656039e2953
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-10-13 14:53:24 -0400

    Improvements made to Header, Layout, and Post componenets

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,19 +1,23 @@
+'use client'
 import Router from 'next/router'
 import ReactMarkdown from 'react-markdown'
 
 const Post = ({ post }) => {
   const authorName = post.author ? post.author.name : 'Unknown author'
   return (
-    <div onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}>
-      <h2>{post.title}</h2>
-      <small>By {authorName}</small>
+    <div className='max-w-md rounded-md p-4 bg-slate-600 hover:bg-slate-400' onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}>
+    <div className=''>
+      <h2 className='text-3xl mb-4'>{post.title}</h2>
+      <h3 className='text-bold mb-'>By {authorName}</h3>
+
+    </div>
       <ReactMarkdown children={post.content} />
-      <style jsx>{`
+      {/* <style jsx>{`
         div {
           color: inherit;
           padding: 2rem;
         }
-      `}</style>
+      `}</style> */}
     </div>
   )
 }
